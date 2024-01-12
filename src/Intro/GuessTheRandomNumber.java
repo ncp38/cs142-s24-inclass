e9f2000cf56730db3998d58a297499f5c63f8959
Nate Phillips <anemailanemailmykingdom@gmail.com>
2024-01-12 10:56:16 -0600

    Update GuessTheRandomNumber.java

--- src/Intro/GuessTheRandomNumber.java
+++ src/Intro/GuessTheRandomNumber.java
@@ -6,7 +6,7 @@ public class GuessTheRandomNumber
         double randomNumber = Math.random();
 
         //Pick a number between 1 and 10
-        int randomInt = (int) Math.random()*10+1;//The +1 changes the result ranging from 0 to 9 to 1 to 10.
+        int randomInt = (int) (Math.random()*10+1);//The +1 changes the result ranging from 0 to 9 to 1 to 10.
 
         //The rest of the program is left for us to work on in class!
     }
