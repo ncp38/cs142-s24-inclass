daa343309abbf268599e0db1bcb58e45d74746a1
Nate Phillips <anemailanemailmykingdom@gmail.com>
2024-01-11 21:22:18 -0600

    Create GuessTheRandomNumber.java

---
+++ src/Intro/GuessTheRandomNumber.java
@@ -0,0 +1,10 @@
+public class GuessTheRandomNumber
+{
+    public static void main(String[] args)
+    {
+        //Between 0 and 1 non-inclusive
+        double randomNumber = Math.random();
+
+        //The rest of the program will be discussed in class!
+    }
+}
