81366a3207d940e0ac6c380bb57836779a416a3a
Nate Phillips <anemailanemailmykingdom@gmail.com>
2024-01-11 22:33:03 -0600

    Update GuessTheRandomNumber.java

--- src/Intro/GuessTheRandomNumber.java
+++ src/Intro/GuessTheRandomNumber.java
@@ -5,6 +5,9 @@ public class GuessTheRandomNumber
         //Between 0 and 1 non-inclusive
         double randomNumber = Math.random();
 
-        //The rest of the program will be discussed in class!
+        //Pick a number between 1 and 10
+        int randomInt = (int) Math.random()*10+1;
+
+        //The rest of the program is left for us to work on in class!
     }
 }
