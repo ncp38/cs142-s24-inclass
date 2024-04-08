86e6f88d83e413f381ccbac2fba5872b52bf44cf
Nate Phillips <anemailanemailmykingdom@gmail.com>
2024-04-08 11:49:53 -0500

    Shape

--- src/poly3/ShapeDemo.java
+++ src/poly3/ShapeDemo.java
@@ -1,5 +1,6 @@
 package poly3;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class ShapeDemo {
@@ -7,5 +8,20 @@ public class ShapeDemo {
         Square sq = new Square(4);
         Triangle tri = new Triangle(3, 5);
         ArrayList<Shape> shapes = new ArrayList<Shape>();
+
+        sq.setColor(Color.CYAN);
+
+        shapes.add(sq);
+        shapes.add(tri);
+
+        for(int i = 0; i < shapes.size(); i++)
+        {
+            shapes.get(i).setColor(Color.CYAN);
+            System.out.println(shapes.get(i).getArea());
+            /*if(shapes.get(i) instanceof Triangle)
+            {
+                ((Triangle) shapes.get(i)).getArea();
+            }*/
+        }
     }
 }
--- src/poly3/Triangle.java
+++ src/poly3/Triangle.java
@@ -1,10 +1,15 @@
 package poly3;
 
-public class Triangle {
+public class Triangle extends Shape {
     private int base, height;
 
     public Triangle(int base, int height) {
         this.base = base;
         this.height = height;
+    }
+
+    public double getArea()
+    {
+        return base * height / 2.0;
     }
 }
