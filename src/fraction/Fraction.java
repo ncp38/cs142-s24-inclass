c03cb08a0cbfc241b3e360403f1c5bfe0657d948
Nate Phillips <anemailanemailmykingdom@gmail.com>
2024-04-17 13:04:10 -0500

    Lab 10

--- src/fraction/Fraction.java
+++ src/fraction/Fraction.java
@@ -2,5 +2,22 @@ package fraction;
 
 public class Fraction {
 
+    int numerator;
+    int denominator;
+    public Fraction(int n, int d)
+    {
+        numerator = n;
+        denominator= d;
+    }
+
+    public Fraction add(Fraction input)
+    {
+        return new Fraction(this.numerator*input.denominator + input.numerator*this.denominator, input.denominator*this.denominator);
+    }
+
+    public String toString()
+    {
+        return "" + numerator + "/" + denominator;
+    }
 
 }
--- src/fraction/FractionDemo.java
+++ src/fraction/FractionDemo.java
@@ -4,10 +4,22 @@ package fraction;
 import java.util.Scanner;
 
 public class FractionDemo {
+
     public static void main(String[] args) {
-        /*Fraction frac = new Fraction(2, 3);
+        Fraction frac = new Fraction(2, 3);
         Fraction frac2 = new Fraction(6, 8);
+        Fraction frac3 = new Fraction(3, 5);
+
         System.out.println(frac);
-        System.out.println(frac2);*/
+
+        Fraction z = frac.add(frac2);
+        Fraction finalZ = frac.add(z);
+        System.out.println(z);
+        System.out.println(finalZ);
+
+
+        z = z.add(frac3);
+        z = z.add(finalZ);
+        System.out.println(z);
     }
 }
